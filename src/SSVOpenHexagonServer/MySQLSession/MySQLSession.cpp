e800cc7f8d6c9f82b539e0c9e85064640c74030a
Vittorio Romeo <vittorio.romeo@outlook.com>
2013-04-09 23:27:36 +0200

    More formatting
    Please review.

--- src/SSVOpenHexagonServer/MySQLSession/MySQLSession.cpp
+++ src/SSVOpenHexagonServer/MySQLSession/MySQLSession.cpp
@@ -1,6 +1,5 @@
 #include "MySQLSession/MySQLSession.hpp"
 
-
 void Concantate(std::string &source)
 {
 	std::string allowedChars{"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890_"};
@@ -9,21 +8,18 @@ void Concantate(std::string &source)
 			source[i] = '_';
 }
 
-
 MySQLSession::MySQLError MySQLSession::getError()
 {
 	MySQLError returnValue;
 	returnValue.errorno  = mysql_errno(connector);
 	returnValue.errorstr = mysql_error(connector);
-	if(verbose)
-		std::cout<<"MySQL Error "<<std::to_string(returnValue.errorno)<<" : "<<returnValue.errorstr<<std::endl;
+	if(verbose) std::cout << "MySQL Error " << std::to_string(returnValue.errorno) << " : " << returnValue.errorstr << std::endl;
 	return(returnValue);
 }
 
 std::string MySQLSession::runQuery(std::string queryString, bool get)
 {
-	if(!initialized)
-		return "";
+	if(!initialized) return "";
 
 	if(mysql_query(connector, queryString.c_str()))
 	{
@@ -32,36 +28,29 @@ std::string MySQLSession::runQuery(std::string queryString, bool get)
 	}
 	//return "";
 
-	if(get)
+	if(!get) return "";
+
+	int num_fields;
+	std::string returnValue;
+
+	result = mysql_store_result(connector); // TODO: initialize instead of assigning?
+	num_fields = mysql_num_fields(result);
+
+	while((row = mysql_fetch_row(result)))
 	{
-		int num_fields;
-
-		std::string returnValue;
-
-		result = mysql_store_result(connector);
-
-		num_fields = mysql_num_fields(result);
-
-		while ((row = mysql_fetch_row(result)))
+		for(int i = 0; i < num_fields; i++)
 		{
-			for(int i = 0; i < num_fields; i++)
-			{
-				if(verbose)
-					std::cout<<(row[i] ? row[i] : "NULL");
-				returnValue += (row[i] ? row[i] : "NULL");
-			}
-			if(verbose)
-				std::cout<<std::endl;
+			if(verbose) std::cout << (row[i] ? row[i] : "NULL");
+			returnValue += (row[i] ? row[i] : "NULL");
 		}
-
-		mysql_free_result(result);
-
-		return returnValue;
+		if(verbose) std::cout << std::endl;
 	}
 
-	return "";
+	mysql_free_result(result);
+	return returnValue;
 }
 
+// TODO: pass strings as const& !!!
 bool MySQLSession::initiate(std::string Hostname_, int port_, std::string username_, std::string password_, std::string database_)
 {
 	Hostname = Hostname_;
@@ -80,18 +69,11 @@ bool MySQLSession::initiate(std::string Hostname_, int port_, std::string userna
 	return initialized;
 }
 
-void MySQLSession::closeSQL()
-{
-	if(!initialized)
-		return;
-	mysql_close(connector);
-}
+void MySQLSession::closeSQL() { if(!initialized) return; mysql_close(connector); }
 
 MySQLSession::MySQLSession(bool verbose_)
 {
 	verbose = verbose_;
 	connector = mysql_init(NULL);
-
-	if(connector == NULL)
-		getError();
+	if(connector == NULL) getError();
 }
